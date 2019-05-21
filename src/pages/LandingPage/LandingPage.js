c65a879b894f34a55e7c55bc00ff5e87fed8d30c
Gautam Tata <gautamtata@gmail.com>
2019-05-21 10:45:11 -0700

    minor changes with fonts and colors

--- src/pages/LandingPage/LandingPage.js
+++ src/pages/LandingPage/LandingPage.js
@@ -3,13 +3,19 @@ import PropTypes from 'prop-types';
 import SocialIcons from '@components/SocialIcons';
 import Nav from '@components/Nav';
 import ScrollToNext from '@components/ScrollToNext';
+import { toElement as scrollToElement } from '@utils/scroll'
 import BrowserNotes from '@components/BrowserNotes';
 
 import './style.scss';
 
-const LandingPage = (props, context) => {
-  const { theme: { bgPrimary, colorPrimary } } = context;
+function scrollToPage(pageSelector){
+  const nextPage = document.querySelector(pageSelector);
+  scrollToElement(nextPage);
+}
 
+const LandingPage = (props, context) => {
+  
+  const { theme: { bgPrimary, colorPrimary } } = context;
 
   return (
     <div style={{ backgroundColor: bgPrimary }} className="landing-page">
@@ -17,11 +23,19 @@ const LandingPage = (props, context) => {
       <main style={{ color: colorPrimary }}>
         <BrowserNotes />
         <div className="intro-wrapper">
-          <div className="intro-name">Hi I am Gautam :)</div>
+          <br />
+          <br />
+          <div className="intro-name">GAUTAM TATA.</div>
             <div className= "intro-name"> 
           <div className="tagline">
-           Full Stack Developer | UI/UX enthusiast | Product builder
+          <b style = {{color: 'yellow', fontSize: 32}}>I'M A</b> Full Stack Developer | UI/UX enthusiast 
+           <br />
+           I've worked on a vareity of <a style = {{color: 'yellow', textDecoration: 'underline', cursor:'pointer'}}onClick={(e) => scrollToPage('.portfolio-page')}>projects</a> in teams and independently. I'm most proud of my <a style = {{color: 'yellow', textDecoration: 'underline',cursor:'pointer'}} href = 'https://js-annotated-maps.herokuapp.com/'>annotatable maps tool;</a>
+           which you can easily use to generate custom maps based on lat-longs with a variety of other features making maps 
+           highly customizable and interactive. <b> <a style = {{color:'yellow', textDecoration: 'underline',cursor:'pointer'}} href="mailto:gautamtata@gmail.com" 
+            target="_top">Looking for exciting new opportunities.</a></b> 
           </div>
+          
           <SocialIcons />
         </div>
        </div> 
